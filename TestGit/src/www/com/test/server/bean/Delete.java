59aaea7648f94ceef1cc7168052cd6d34685d015
Administrator <Administrator@PC-201703131734>
2018-09-15 10:25:20 +0800

    新增加一个文件

---
+++ TestGit/src/www/com/test/server/bean/Delete.java
@@ -0,0 +1,16 @@
+package www.com.test.server.bean;
+
+public class Delete {
+	
+	private String fileName ;
+
+	public String getFileName() {
+		return fileName;
+	}
+
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+	
+	
+}
